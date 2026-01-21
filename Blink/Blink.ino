5ab863d468e17086fa13855bd4b68f70468e1a5e
koblume <koblume@ilstu.edu>
2026-01-21 15:24:15 -0600

    Added a while loop
    The LED never stops blinking because the value of timesToBlink is always greater than zero. This condition is an infinite loop.

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -40,12 +40,20 @@ void setup() {
     //digitalWrite(LED_BUILTIN, HIGH);
   //}
 
-  for(int i = 0; i < timesToBlink; i++){
+  //for(int i = 0; i < timesToBlink; i++){
+    //digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+    //delay(1000);                      // wait for a second
+    //digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
+    //delay(1000);                      // wait for a second
+  //}
+
+  while(timesToBlink > 0){
     digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
     delay(1000);                      // wait for a second
     digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
     delay(1000);                      // wait for a second
   }
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 // the loop function runs over and over again forever
